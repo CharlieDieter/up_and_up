1d6506b16d9238861fd3d15029d6f2b7df028717
Charlie Dieter <charlie.dieter@gmail.com>
2018-05-22 10:29:26 -0400

    Install ColorHash for unique line colors

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { parseTime } from "../util/d3_util.js";
+import { scaleLinear, scaleTime } from "d3-scale";
+import { line } from "d3-shape";
+import { select } from "d3-selection";
+import ColorHash from "color-hash";
 
 class LineGraph extends Component {
   static defaultProps = {
@@ -14,13 +18,33 @@ class LineGraph extends Component {
     data: {}
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.createLineGraph();
+  }
 
   createLineGraph() {
+    const colorHash = new ColorHash();
     const node = this.graphNode;
     const { margins, width, height, data } = this.props;
-    const width = width - margin.left - margin.right;
-    const height = height - margin.top - margin.bottom;
+
+    const x = scaleTime([0, width]);
+    const y = scaleLinear([height, 0]);
+
+    const valueLine = line()
+      .x(d => x(d.date))
+      .y(d => y(d.close));
+
+    // TODO: add margins
+    const svg = select(this.graphNode)
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${margins.left} ${margins.top})`);
+
+    Object.keys(data).forEach(symbol => {
+      console.log(colorHash.hex(symbol));
+      svg.append("path").data([data[symbol]]);
+    });
   }
 
   render() {
