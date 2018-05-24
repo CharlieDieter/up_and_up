aa2e0a65bb083473f0a76f760f9cf688875eff6b
Charlie Dieter <charlie.dieter@gmail.com>
2018-05-23 22:54:30 -0400

    Change loader image to work in production

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,10 @@ import "../styles/Loader.css";
 const Loader = props => {
   return (
     <div className="load-screen">
-      <img id="loader" src="/favicon.png" />
+      <img
+        id="loader"
+        src="http://www.emoji.co.uk/files/mozilla-emojis/objects-mozilla/11933-chart-with-upwards-trend.png"
+      />
     </div>
   );
 };
